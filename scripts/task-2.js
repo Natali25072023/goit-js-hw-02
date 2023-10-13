196baafa35d5925331c6720e449d5afd8169cf27
Natali25072023 <infkab2@gmail.com>
2023-10-13 10:03:05 +0300

    vers-1

--- scripts/task-2.js
+++ scripts/task-2.js
@@ -1,6 +1,10 @@
-function getShippingMessage(country, price, deliveryFee) { 
-    console.log(`Shipping to ${country} will cost ${price+deliveryFee} credits`);
-}
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
+function formatMessage(message, maxLength) { 
+    if (message.length <= maxLength) {
+        console.log(message);
+    }
+    else {
+            
+     }
+     }
+
+formatMessage("Curabitur ligula sapien", 23);
